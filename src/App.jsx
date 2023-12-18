d9684bc092e0f9a1b1a5df16464826b48a42fc67
imronbekgaipov <imronbekgaipov2008@gmail.com>
2023-12-18 15:43:31 +0500

    finish

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navbar from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main className="max-container">
+      <h2>hello world</h2>
+      <div className="">
+        <h1>Join our community</h1>
+      </div>
+      <div className="flex">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
